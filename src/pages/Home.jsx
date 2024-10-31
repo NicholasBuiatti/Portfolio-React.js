b694303e97efef6ff482e45dfa8b835e4ae8fc62
NicholasBuiatti <nic.buiatti@gmail.com>
2024-10-31 12:37:09 +0100

    font family

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className='' style={{ 'height': '120vh' }}>Home</div>
+        <section className='bg-white'>
+            <div className='text-center p-6 w-6/12'>
+                <h1 className='text-3xl mb-6'>Jr. Full Stack Web Developer</h1>
+                <p>Benvenuto nel mio portfolio personale, creato per affinare le mie
+                    competenze come Full Stack Web Developer. Qui puoi esplorare i progetti che ho sviluppato, con
+                    una
+                    particolare attenzione sia al front-end che al back-end. Per ogni progetto, ho implementato e
+                    utilizzato API costruite su misura, con cicli di interazione dati completi.
+
+                    Oltre a questo, ho realizzato una piattaforma di gestione progetti accessibile esclusivamente
+                    dall'amministratore, per garantire il controllo e l'organizzazione delle attività in maniera
+                    efficiente.
+
+                    Esplora i miei lavori e scopri come posso contribuire con le mie capacità di sviluppo web a
+                    progetti
+                    innovativi.</p>
+            </div>
+            <div className='w-6/12'>
+
+            </div>
+        </section>
     )
 }
 
