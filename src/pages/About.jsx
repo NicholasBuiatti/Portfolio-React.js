bea30b02e49d06876b37602271bb32646939fa3e
Nicholas Buiatti <nic.buiatti@gmail.com>
2025-08-05 23:14:43 +0200

    rimodellato il layout e pensiero di togliere quella schifezza di carosello

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,31 +12,24 @@ import VuePng from "../assets/Vue.png";
 const About = () => {
   return (
     <>
-      <section className="text-white my-6 " id="sfondoAbout">
-        <div className="bg-black/[.54]">
-          <div className=" md:w-8/12 py-10 px-10">
-            <h1 className="text-3xl md:text-6xl mb-4">
-              Ecco qualche informazione in più.
-            </h1>
+      <section className="container mx-auto">
+        <div className="flex justify-between items-center py-10 border-b">
+          <div className=" md:w-5/12 py-10 px-10">
+            <h1 className="text-7xl text-gray-800 font-bold mb-4">about.</h1>
+            <h3 className="text-xl text-gray-500 mb-2">
+              Sono un Full Stack Developer che risiede nel nord Italia
+            </h3>
             <p className="text-sm md:text-base">
-              Sono Nicholas Buiatti, un Junior Full Stack Web Developer
-              all'inizio del mio percorso professionale, ma con una grande
-              voglia di imparare e crescere. Specializzato nello sviluppo sia
-              lato front-end che back-end, mi dedico con passione a realizzare
-              progetti solidi e funzionali, curando ogni dettaglio per offrire
-              esperienze utente intuitive. Oltre alla programmazione, nutro una
-              forte passione per la pallavolo e per l'attività all'aria aperta,
-              che mi aiutano a mantenere un equilibrio tra lavoro e vita
-              personale. Apprezzo profondamente il tempo trascorso con gli
-              amici, poiché credo che le relazioni significative arricchiscano
-              la vita e stimolino la creatività. Sono sempre pronto a esplorare
-              nuove tecnologie e sfide, motivato dalla voglia di innovare e
-              imparare.
+              Dal 2024 ho intrapreso un percorso nel mondo dell’informatica,
+              dedicandomi con entusiasmo alla programmazione full stack. Se non
+              sono al computer puoi trovarmi in palestra, a giocare a beach
+              volley o a passare del tempo con gli amici.
             </p>
           </div>
+          <div className=" md:w-7/12 py-10 px-10">qua ci va l'immagine</div>
         </div>
+        {/* <Languages /> mi sa che non la metto più perchè fa schifo */}
       </section>
-      <Languages />
     </>
   );
 };
@@ -55,7 +48,7 @@ const Languages = () => {
   const containerRef = useRef(null);
   const [width, setWidth] = useState(0);
 
-  const allLogos = [...logos, ...logos, ...logos];
+  const allLogos = [...logos, ...logos, ...logos, ...logos];
 
   useEffect(() => {
     if (containerRef.current) {
