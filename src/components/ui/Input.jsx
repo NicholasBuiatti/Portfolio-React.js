18047f4db0dd9baf86fa62129f824282cd5dfac4
Nicholas Buiatti <160488490+NicholasBuiatti@users.noreply.github.com>
2025-08-08 10:10:10 +0200

    aggiornamento componente per cambio tag

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,22 +1,17 @@
 import PropTypes from "prop-types";
 
-export default function Input({
-  label,
-  type = "text",
-  value,
-  onChange,
-  name,
-  ...props
-}) {
+export default function Input({ label, type = "text", value, onChange, name, as, ...props }) {
+  const Tag = as === "textarea" ? "textarea" : "input";
+
   return (
     <div className="w-full mb-5 group">
-      {label && <label className="">{label}</label>}
-      <input
-        type={type}
+      {label && <label className="text-gray-600">{label}</label>}
+      <Tag
+        type={Tag === "input" ? type : undefined}
         name={name}
         value={value}
         onChange={onChange}
-        className="block py-2.5 px-0 w-full"
+        className="block w-full border rounded-md border-gray-700 py-2.5 px-1"
         {...props}
       />
     </div>
@@ -29,4 +24,5 @@ Input.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func,
   name: PropTypes.string,
+  as: PropTypes.string,
 };
