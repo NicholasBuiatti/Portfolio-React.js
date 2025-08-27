bb58ee8518ab03b2609d75796a951721b687a9a7
Nicholas Buiatti <160488490+NicholasBuiatti@users.noreply.github.com>
2025-08-27 16:48:24 +0200

    aggiornamento filtri

--- src/features/projects/components/ProjectsFilters.jsx
+++ src/features/projects/components/ProjectsFilters.jsx
@@ -3,11 +3,18 @@ import { useProjects } from "../hooks";
 import Input from "../../../components/ui/Input";
 import Select from "../../../components/ui/Select";
 import { useState } from "react";
+import Button from "../../../components/ui/Button";
+import PropTypes from "prop-types";
 
 const ProjectsFilters = ({ page }) => {
-  const [form, setForm] = useState({});
-  const [filters, setFilters] = useState(form);
-  const { data, isLoading, error } = useProjects(page, filters);
+  const [form, setForm] = useState({
+    name_project: "",
+    type: [],
+    language: ""
+  });
+  const [filters, setFilters] = useState({});
+  const queryParams = { ...filters, page };
+  const { data, isLoading, error } = useProjects(queryParams);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,55 +23,71 @@ const ProjectsFilters = ({ page }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      {/* Qui puoi aggiungere i filtri per i progetti */}
-      <h2>Filtri Progetti</h2>
-      <Input
-        label={"Nome Progetto"}
-        name="name_project"
-        placeholder="Cerca progetto..."
-        value={form.name_project}
-        onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
-      />
+    <form onSubmit={handleSubmit} className="flex justify-between items-end">
+      <div className="md:w-1/3">
+        <Input
+          label={"Nome Progetto"}
+          name="name_project"
+          placeholder="Cerca progetto..."
+          value={form.name_project}
+          onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
+        />
+      </div>
 
-      {/* VEDERE DELLA SELEZIONE MULTIPLA ANCHE LATO BACKEND */}
-      <Select
-        endpoint={getTypes}
-        value={form.type}
-        name="type"
-        multiple
-        onChange={(e) =>
-          setForm({
-            ...form,
-            [e.target.name]: Array.from(
-              e.target.selectedOptions,
-              (option) => option.value
-            ),
-          })
-        }
-        mapOptions={(data) =>
-          data.types.map((type) => ({
-            value: type.id,
-            label: type.name,
-          }))
-        }
-      />
+      {/* TODO SELEZIONE MULTIPLA LATO BACKEND */}
+      <div className="w-1/2 md:w-1/6">
+        <Select
+          label={"Tipo"}
+          endpoint={getTypes}
+          value={form.type}
+          name="type"
+          multiple
+          onChange={(e) =>
+            setForm({
+              ...form,
+              [e.target.name]: Array.from(
+                e.target.selectedOptions,
+                (option) => option.value
+              ),
+            })
+          }
+          mapOptions={(data) =>
+            data.types.map((type) => ({
+              value: type.id,
+              label: type.name,
+            }))
+          }
+        />
+      </div>
 
-      <Select
-        endpoint={getLanguages}
-        value={form.language}
-        name="language"
-        onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
-        mapOptions={(data) =>
-          data.languages.map((language) => ({
-            value: language.id,
-            label: language.name,
-          }))
-        }
-      />
-      <button type="submit">Filtra</button>
+      <div className="w-1/2 md:w-1/6">
+        <Select
+          label={"Linguaggio"}
+          endpoint={getLanguages}
+          value={form.language}
+          name="language"
+          onChange={(e) => setForm({ ...form, [e.target.name]: e.target.value })}
+          mapOptions={(data) =>
+            data.languages.map((language) => ({
+              value: language.id,
+              label: language.name,
+            }))
+          }
+        />
+      </div>
+      <div>
+        <Button
+          text={"Filtra"}
+          variant="outline"
+          className="mb-5"
+        />
+      </div >
     </form>
   );
+};
+
+ProjectsFilters.propTypes = {
+  page: PropTypes.number.isRequired,
 };
 
 export default ProjectsFilters;
