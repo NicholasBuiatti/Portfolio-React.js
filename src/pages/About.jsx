cd3df9f97e9570c24861b0307c5b290ecdfef227
Nicholas Buiatti <160488490+NicholasBuiatti@users.noreply.github.com>
2025-08-28 11:55:05 +0200

    modifica intera pagina

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import StartingPage from "../components/ui/StartingPage";
 import Section from "../components/ui/Section";
+import Card from "../components/common/Card";
+import { useInView } from "react-intersection-observer";
 
 import ReactPng from "../assets/React.png";
 import HtmlPng from "../assets/Html.png";
@@ -12,55 +14,34 @@ import LaravelPng from "../assets/Laravel.png";
 import VuePng from "../assets/Vue.png";
 
 import Yoda from "../assets/yoda.jpg";
+import Prova from "../assets/prova.webp";
+import Prova1 from "../assets/prova1.jpg";
+import Prova2 from "../assets/prova2.jpg";
+import Prova3 from "../assets/prova3.jpg";
+import Prova4 from "../assets/prova4.jpg";
+import Torta60 from "../assets/torta60.png";
 
 const About = () => {
   return (
     <>
       <Section>
         <div className="max-w-6xl mx-auto">
-
           <StartingPage
-            title="About Me"
+            title="about me."
             semiTitle="Sono un Full Stack Developer residente in un bellissimo paesino friulano."
             description="Dal 2024 ho intrapreso un percorso nel mondo dell’informatica,
           dedicandomi con entusiasmo alla programmazione full stack. Se non
           sono al computer puoi trovarmi in palestra, a giocare a beach
           volley o a passare del tempo con gli amici."
-            reverse={true}
+            image={<img src={Prova} alt="Prova" />}
           />
+          <hr />
+          <ExpoImage />
         </div>
       </Section>
       <div className="p-10">
-        <div className="max-w-6xl mx-auto flex items-center text-center">
-          <div className="md:w-1/3">
-            <h3 className="text-lg font-semibold">Front End</h3>
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>React</li>
-              <li>Vue</li>
-              <li>Bootstrap</li>
-              <li>Tailwind CSS</li>
-            </ul>
-          </div>
-          <div className="md:w-1/3">
-            <h3 className="text-lg font-semibold">Tools & Version Control</h3>
-            <ul>
-              <li>Git</li>
-              <li>GitHub</li>
-              <li>VS Code</li>
-              <li>npm/yarn</li>
-            </ul>
-          </div>
-          <div className="md:w-1/3">
-            <h3 className="text-lg font-semibold">Back End</h3>
-            <ul>
-              <li>PHP</li>
-              <li>Laravel</li>
-              <li>MySQL</li>
-            </ul>
-          </div>
+        <div className="max-w-6xl mx-auto flex flex-wrap items-center">
+          <SkillList />
         </div>
       </div>
       <Section>
@@ -70,7 +51,9 @@ const About = () => {
             description="Sono sempre alla ricerca di nuove tecnologie e strumenti per migliorare il mio flusso di lavoro e le mie competenze."
             image={<img src={Yoda} alt="Yoda" />}
             className="pb-0"
+            reverse={true}
           />
+          <hr />
         </div>
       </Section>
       <div className="p-10">
@@ -78,7 +61,7 @@ const About = () => {
           <StartingPage
             semiTitle="Non lo so"
             description="Questa è una sezione di esempio."
-            reverse={true}
+
           />
         </div>
       </div>
@@ -88,6 +71,59 @@ const About = () => {
 
 export default About;
 
+const ExpoImage = () => {
+  const Image = [
+    { src: Prova1, alt: "HTML" },
+    { src: Prova2, alt: "CSS" },
+    { src: Prova3, alt: "JavaScript" },
+    { src: Prova4, alt: "Vue" },
+    { src: Prova, alt: "React" },
+  ];
+
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-100px",
+  });
+
+  return (
+    <div className="flex flex-wrap justify-around align-center gap-6 mt-2" ref={ref}>
+      {Image.map((img, idx) => (
+        <>
+          <Card image={img.src} alt={img.alt} inView={inView} delay={idx * 0.15} />
+        </>
+      ))}
+    </div>
+  )
+}
+
+const SkillList = () => {
+  return (<>
+    <div className="w-full md:w-1/4 text-center md:text-start order-2 md:order-1">
+      <h3 className="text-3xl text-gray-800 font-bold">Parte Backender</h3>
+      <ul className="text-xl text-gray-500">
+        <li>PHP</li>
+        <li>Laravel</li>
+        <li>MySQL</li>
+      </ul>
+    </div>
+    <div className="mx-auto w-1/2 order-3 md:order-2">
+      <img src={Torta60} alt="Torta 40-60" className="w-full h-auto" />
+    </div>
+    <div className="w-full md:w-1/4 text-center md:text-end order-1 md:order-3 mb-3 md:mb-0">
+      <h3 className="text-3xl text-gray-800 font-bold">Parte Frontender</h3>
+      <ul className="text-xl text-gray-500">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Vue</li>
+        <li>Bootstrap</li>
+        <li>Tailwind CSS</li>
+      </ul>
+    </div>
+  </>
+  );
+};
 
 //////////////////////////////////////////////////////// DA TOGLIERE
 const logos = [
