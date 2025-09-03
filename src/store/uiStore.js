86303fecc719cb7cdaae7c220b8df2ca985ea3ee
Nicholas Buiatti <160488490+NicholasBuiatti@users.noreply.github.com>
2025-09-03 17:28:56 +0200

    change

--- src/store/uiStore.js
+++ src/store/uiStore.js
@@ -1,11 +1,9 @@
 import { create } from "zustand";
 
-export const useUIStore = create((set) => ({
+export const useUiStore = create((set) => ({
   isDarkMode: false,
   toggleDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
-}));
-
-export const useUiStore = create((set) => ({
+  
   navbarDropdown: false,
   // otherDropdown: false, //ci fossero altri dropdown
   toggleNavbarDropdown: () => set((state) => ({ navbarDropdown: !state.navbarDropdown })),
